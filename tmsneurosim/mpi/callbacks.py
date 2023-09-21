2529285ced5e0df7b3e2ecbaf24be990d29d78fc
Minhaj Hussain <minhaj.hussain@duke.edu>
2023-09-21 10:14:56 -0400

    callback save name error solve

--- tmsneurosim/mpi/callbacks.py
+++ tmsneurosim/mpi/callbacks.py
@@ -633,7 +633,7 @@ class PredictedInitGeometryRecorder(ThresholdCallback):
                 ints.append(h.distance(terminal_sec(0.5), nn(0.5)) / d_t_s)
                 i += 1
 
-        self.save('pred_axon_first_internode_dist_norm', i, j, k, ints)
+        self.save('pred_axon_internode_dist_norm', i, j, k, ints)
 
         # a_f_i_n = h.distance(apic_sec(0.5), apic_branch[-2](0.5)) / d_a_s
         # self.save('pred_apic_first_internode_dist_norm', i, j, k, a_f_i_n)
